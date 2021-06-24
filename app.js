f55e145a31b66bb86041e0c370bd5dea14ce39fc
Joaquin <andres16081990@gmail.com>
2021-06-24 09:57:55 -0500

    solucion

---
+++ app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('<div class="secret" style="display: none;">HcUy6Re2LLBRtj</div>')
+})
+
+app.listen(3000, () => console.log('Listening on port 3000!'));
+
