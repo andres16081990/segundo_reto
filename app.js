c605c065bce5dea80a8f75f5bb896a105dac1be8
Joaquin <andres16081990@gmail.com>
2021-06-24 10:08:19 -0500

    fix

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 
 app.get('/',(req,res)=>{
-    res.send('<div class="secret" style="display: none;">HcUy6Re2LLBRtj</div>')
+    res.send('<h1>HcUy6Re2LLBRtj</h1')
 })
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
